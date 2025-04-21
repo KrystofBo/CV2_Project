f1d7c367bfc9eaf28a16bc666c2242bec40b3d6e
Karolina Hajkova <karolinahajkova00@gmail.com>
2025-04-21 10:43:12 +0200

    Semantic segmentation scripts

--- segmenter_masks.py
+++ segmenter_masks.py
@@ -1,4 +1,3 @@
-# run_segformer_generate_masks.py
 from mmseg.apis import init_model, inference_model
 import mmcv
 import os
@@ -6,9 +5,8 @@ import numpy as np
 from PIL import Image
 import torch
 
-config_file = 'mmsegmentation/configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512.py'
-checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/segformer/segformer_mit-b0_512x512_160k_ade20k/segformer_mit-b0_512x512_160k_ade20k_20210726_101530-8ffa8fda.pth'
-
+config_file = 'mmsegmentation/configs/segmenter/segmenter_vit-b_mask_8xb1-160k_ade20k-512x512.py'
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/segmenter/segmenter_vit-b_mask_8x1_512x512_160k_ade20k/segmenter_vit-b_mask_8x1_512x512_160k_ade20k_20220105_151706-bc533b08.pth'
 
 model = init_model(config_file, checkpoint_file, device='cuda:0')
 
