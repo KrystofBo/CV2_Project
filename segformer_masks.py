230902c950ed7f02c6c2ff1a2f510c266557f53e
Krystof Bobek <scur0680@int5.local.snellius.surf.nl>
2025-05-08 09:35:10 +0200

    job files

--- segformer_masks.py
+++ segformer_masks.py
@@ -6,14 +6,13 @@ import numpy as np
 from PIL import Image
 import torch
 
-config_file = 'mmsegmentation/configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512.py'
-checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/segformer/segformer_mit-b0_512x512_160k_ade20k/segformer_mit-b0_512x512_160k_ade20k_20210726_101530-8ffa8fda.pth'
-
+config_file = 'mmsegmentation/configs/segformer/segformer_mit-b5_8xb2-160k_ade20k-512x512.py'
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/segformer/segformer_mit-b5_512x512_160k_ade20k/segformer_mit-b5_512x512_160k_ade20k_20210726_145235-94cedf59.pth'
 
 model = init_model(config_file, checkpoint_file, device='cuda:0')
 
-img_dir = 'gaussian-grouping/data/bear/images'
-output_dir = 'gaussian-grouping/data/bear/object_mask_segmenter'
+img_dir = 'gaussian-grouping/data/office/images'
+output_dir = 'gaussian-grouping/data/office/object_mask_segformer'
 os.makedirs(output_dir, exist_ok=True)
 
 for img_name in sorted(os.listdir(img_dir)):
